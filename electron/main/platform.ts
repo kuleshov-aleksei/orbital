3b4631d63f1451b14268d2865209711a5e0d3e5b
kuleshov-aleksei <alexxey67@gmail.com>
2026-08-11 19:25:11 +0100

    electron: clean up app flags

--- electron/main/platform.ts
+++ electron/main/platform.ts
@@ -2,38 +2,31 @@ import { app } from "electron"
 import log from "electron-log"
 
 // Media control keys can interfere with media objects in the app
-app.commandLine.appendSwitch("disable-features", "HardwareMediaKeyHandling,MediaSessionService");
+app.commandLine.appendSwitch("disable-features", "HardwareMediaKeyHandling,MediaSessionService,WebRtcAllowInputVolumeAdjustment")
 
 if (process.platform === "win32") {
-  app.commandLine.appendSwitch("enable-features", "DesktopCaptureWinDXGICapturer")
   app.commandLine.appendSwitch("disable-gpu-sandbox")
   app.commandLine.appendSwitch("ignore-gpu-blocklist")
-  app.commandLine.appendSwitch("enable-gpu")
-  app.commandLine.appendSwitch("enable-hardware-overlays", "single-fullscreen,single-on-top,underlay")
-  app.commandLine.appendSwitch("enable-dxgi-mode", "win32")
-  app.commandLine.appendSwitch("disable-dxgi-zero-copy")
-  app.commandLine.appendSwitch("enable-webgl")
   app.commandLine.appendSwitch("use-angle", "d3d11")
 
   // https://github.com/electron/electron/issues/2237#issuecomment-126542840
   app.commandLine.appendSwitch("enable-usermedia-screen-capturing")
 } else if (process.platform === "linux") {
   app.commandLine.appendSwitch("enable-features", "GlobalShortcutsPortal,PipeWireCapturer")
-  app.commandLine.appendSwitch("disable-features", "WebRtcAllowInputVolumeAdjustment");
 }
 
 app.commandLine.appendSwitch("webrtc-max-cpu-consumption-percentage", "100")
-app.commandLine.appendSwitch("max-gum-fps", "120")
-app.commandLine.appendSwitch("webrtc-max-capture-framerate", "120")
-
-app.commandLine.appendSwitch("enable-gpu-compositing")
+app.commandLine.appendSwitch("max-gum-fps", "60")
+app.commandLine.appendSwitch("webrtc-max-capture-framerate", "60")
 
 export const isWayland = process.platform === "linux" && 
   (process.env.XDG_SESSION_TYPE === "wayland" || 
    process.env.WAYLAND_DISPLAY !== undefined ||
    process.argv.includes("--ozone-platform=wayland"))
 
-log.info("[Platform] Running on Wayland:", isWayland)
+if (process.platform === "linux") {
+  log.info("[Platform] Running on Wayland:", isWayland)
+}
 
 export const isDebugMode = process.argv.includes("--debug") || process.argv.includes("--devtools")
 
